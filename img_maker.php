49584c402c9b8d693d2212a7c75b0973e838c237
emuroku <m610310@gmail.com>
2021-08-24 14:31:43 +0900

    プロトタイプ作成

---
+++ img_maker.php
@@ -0,0 +1,6 @@
+<?php
+
+$filepath = 'img/tpl_p.png';
+$base_data = file_get_contents($filepath);
+
+include_once 'result.php';
---
+++ result.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Bsメッセージタオル風イメージメーカー</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+        integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" href="index.css">
+</head>
+
+<body>
+    <!-- navigation -->
+    <nav class="navbar justify-content-center navbar-light bg-light">
+        <h1>★Bsメッセージタオル風イメージメーカー★</h1>
+    </nav>
+
+    <!-- container -->
+    <div class="container">
+        <div class=></div>
+        <img class="main_img" src= <?php print($filepath); ?> width=400px>
+
+</body>
+
+</html>
